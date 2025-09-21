6a252c7018ec4f7f3e42f8dbc19eac6dd2b72b1c
snezhka003 <you@example.com>
2025-09-21 17:54:01 +0300

    Add solving the task

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+
+//        System.out.println("Начислено миль за билет: " + miles);
+        return price / 20;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
 
+        int price = 565;
+        int miles = service.calculate(price);
+
+        System.out.println(miles);
     }
 }
